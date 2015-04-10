b5a1470a1df87370f6e68fd55287e1c7ddd8afd4
srinivasprv <srinivasprv@gmail.com>
2015-04-10 15:04:16 +0530

    hw: Support for SABRE Lite Contributed by the Department of Computer Sience & Engg., IIT Madras.

---
+++ repos/base-hw/mk/spec-hw_sabrelite.mk
@@ -0,0 +1,24 @@
+#
+# \brief  Build configurations for 'base-hw' on SABRE Lite
+# \author Nikolay Golikov <nik@ksyslabs.org>
+# \author Josef Soentgen
+# \author Martin Stein
+# \author Praveen Srinivas (IIT Madras, India)
+# \date   2014-02-25
+#
+
+# denote wich specs are also fullfilled by this spec
+SPECS += hw platform_imx6
+
+# configure multiprocessor mode
+NR_OF_CPUS = 1
+
+# set address where to link the text segment at
+LD_TEXT_ADDR ?= 0x10001000
+
+# add repository relative include paths
+REP_INC_DIR += include/platform/sabrelite
+
+# include implied specs
+include $(call select_from_repositories,mk/spec-hw.mk)
+include $(call select_from_repositories,mk/spec-platform_imx6.mk)
---
+++ repos/base/include/platform/sabrelite/drivers/board_base.h
@@ -0,0 +1,46 @@
+/*
+ * \brief  Board definitions for specific for SABRE Lite
+ * \author Nikolay Golikov <nik@ksyslabs.org>
+ * \author Josef Soentgen
+ * \author Martin Stein
+ * \author Praveen Srinivas (IIT Madras, India)
+ * \date   2014-02-25
+ */
+
+/*
+ * Copyright (C) 2014-2015 Ksys Labs LLC
+ * Copyright (C) 2014-2015 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU General Public License version 2.
+ */
+
+#ifndef _SABRELITE__DRIVERS__BOARD_BASE_H_
+#define _SABRELITE__DRIVERS__BOARD_BASE_H_
+
+/* Genode includes */
+#include <platform/imx6/drivers/board_base.h>
+
+namespace Genode
+{
+	/**
+	 * Motherboard
+	 */
+	struct Board_base;
+}
+
+struct Genode::Board_base : Imx6::Board_base
+{
+	enum {
+		/* normal RAM */
+		RAM0_BASE = 0x10000000,
+		RAM0_SIZE = 0x40000000,
+
+		/* UART */
+		UART_1_IRQ       = 57,
+		UART_1_MMIO_BASE = 0x021e8000,
+		UART_1_MMIO_SIZE = 0x00004000,
+	};
+};
+
+#endif /* _SABRELITE__DRIVERS__BOARD_BASE_H_ */
